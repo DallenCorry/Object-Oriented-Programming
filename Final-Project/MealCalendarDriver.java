ddeb06efe6e2102b3285a4e15de319ab12bc1b43
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2021-10-13 18:15:24 -0600

    Update MealCalendarDriver.java

--- Final-Project/MealCalendarDriver.java
+++ Final-Project/MealCalendarDriver.java
@@ -4,106 +4,187 @@ import java.io.*;
 import java.util.*;
 
 /**
- * This class is the driver for the Meal Calendar project. It creates a calendar for one week of meals to eat.
+ * The driver for the Meal Calendar project, Creates a calendar for one week of meals. 
+ * Created 9/29/2021
  * @author Dallen Corry
- * @version 1.0
- * @since 9/29/2021
- */
+ * @version 1.9
+ * @since 10/13/2021
+ */ 
 public class MealCalendarDriver {
 	/*TODO
 	 * URGENT
+ 	 * [X]implement randomization for Days creating meals.
+ 	 * 		[~]If a that number already exists in meals, don't use it(ie don't repeat meals)
+	 * [X]implement Shopping List as an array list
 	 * [X]implement adding ingredients to IL from meal.java
-	 * [ ]implement Shopping List as an array list
 	 * [X]implement toString for IngredientsList
-	 * [ ]implement randomization for Days creating meals.
+
 	 * 
 	 * [X]create 7 days with a meal
 	 * [X]Create printing method.
+	 * [ ]Make every recipe standard in units of measurement
+	 * 		I.e, all chicken must be measured in pounds.
 	 * 
-	 * [ ]change the meal object in day to an arraylist of meals
 	 * 
 	 * Other
+	 * [ ]Print starting selection different if there is a menu created or not
 	 * [ ]implement date element of Day
 	 *		[ ] Implement a start date and calculate out x amount of days
-	 * 			use Calendar cal = new GregorianCalendar (2021,9,1) (9 is october, because jan is 0)
+	 * 			use Calendar cal = new GregorianCalendar (2021,9,1) (9 is October, because Jan. is 0)
 	 * 
+	 * Future Expansion?
+	 * [ ]Change the meal object in Day to an arrayList of Meal Objects
+	 * [ ]Make it possible to change the meal of a certain day
+	 * [ ]Make a program to be able to input a recipe and format it correctly. 
 	 * */
 	
-	public static void printWeek(Week w) {
-		System.out.println(w.toStringWithoutIngredients());//obviously could be called in the main, but this method is here for future upscaling.
-		
-		//somehow make the lines on top and bottom print out at a certain number of chars or something.
-//		System.out.println(" __________________________________________________________________________________________________________ ");
-//		System.out.println("|                                                                                                          |");
-//		System.out.print("| 1."+w.mealName(0,0)+" | 2."+w.mealName(1,0)+" | 3."+w.mealName(2, 0)+" | 4."+w.mealName(3,0)+
-//				" |5."+w.mealName(4, 0)+" | 6."+w.mealName(5, 0)+" | 7."+w.mealName(6, 0));
-//		System.out.println(" |");
-//		System.out.println("|__________________________________________________________________________________________________________|");
+	/**
+	 * Prints out the menu for a week in a formated manner
+	 * @param w (Week) The week to be printed out. 
+	 */
+	public static void printWeek(Week w) {		
+		String s = "| 1."+w.mealName(0,0)+" | 2."+w.mealName(1,0)+" | 3."+w.mealName(2, 0)+" | 4."+w.mealName(3,0)+" | 5."+w.mealName(4, 0)+" | 6."+w.mealName(5, 0)+" | 7."+w.mealName(6, 0)+" |";
+		System.out.print(" ");
+		for(int i=0; i<s.length()-2;i++)
+			System.out.print("_");
+		System.out.println();
+		System.out.print("|");
+		for(int i=0; i<s.length()-2;i++)
+			System.out.print(" ");
+		System.out.println("|");
+		for( int i=0; i<7;i++) {
+			System.out.print("| "+(i+1)+"."+w.mealName(i, 0)+" ");
+		}
+		System.out.println("|");
+		System.out.print("|");
+		for(int i=0; i<s.length()-2;i++)
+			System.out.print("_");
+		System.out.print("|");
+		System.out.println();
 	}
 	
-	private static Week generateNewMenu(File f) {
-		int random = (int)Math.floor(Math.random()*10);
-		System.out.println(random);
-		Day d1 = new Day(1,f);
-		//Week w = new Week(d1,d2,d3,d4,d5,d6,d7);
-		return null;
+	/**
+	 * Generates a new menu from a given file of recipes.
+	 * @param f (File) The file containing lines of recipes
+	 * @return (Week) A week with 7 days of meals created from the specified file
+	 */
+	public static Week generateNewMenu(File f) {
+		try {
+			Day[] d = new Day[7];
+			int random =0;
+			int count=0;
+			int[] test = new int[7];
+			Scanner input = new Scanner(f);
+			while(input.hasNextLine()) {
+				count++;
+				input.nextLine();
+			}
+			for(int i=0; i<d.length;i++) {
+				random = (int)Math.floor(Math.random()*count);
+				//test if the meal is a repeat.
+				if(!contains (test,random)) {
+					d[i] = new Day(random+1,f);
+					test[i]=random;
+				}else {
+					random = (int)Math.floor(Math.random()*count);
+					d[i] = new Day(random+1,f);
+					test[i]=random;
+				}	
+			}
+			Week w = new Week(d);
+			return w;
+		}catch(Exception E) {
+			System.out.println(E);//"Bad file!");
+			return new Week();
+		}
+	}
+	/**
+	 * Searches an int array for a specific number.
+	 * @param arr (int[]) The array being searched.
+	 * @param x (int) The int being searched for.
+	 * @return True if the array contains x, false otherwise.
+	 */
+	public static boolean contains(int[] arr, int x) {
+		for (int i=0; i<arr.length; i++) {
+			if(arr[i]==x)
+				return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Main method. Uses the file Ingredients.txt
+	 * @param args (String[]) Unused
+	 */
 	public static void main(String[] args) {
 		try {
 			boolean active = true;
 			Scanner input = new Scanner(System.in);
-			
-			//get the ingredients file
 			File f = new File("Ingredients.txt");
-			Day day = new Day(1,f);
-			Day day2 = new Day(2,f);
-			Day day3 = new Day(3,f);
+			Week w = null;			
 			
-			Week w1 = new Week();
-			w1.setDay(0, day);
-			w1.setDay(1, day2);
-			w1.setDay(2, day3);
-			w1.setDay(3, day);
-			w1.setDay(4, day2);
-//			w1.setDay(5, day3);
-			w1.setDay(6, day3);
-			
-		
 			System.out.println("Welcome to Dallen Corry's Meal Calendar");
+			System.out.println("_______________________________________");
+			
 			while(active) {
+				System.out.println();
 				System.out.println("What would you like to do?");
 				System.out.println(" 1. Create new Menu");
-				System.out.println(" 2. View Recipe List");
+				System.out.println(" 2. View current Menu");
+				System.out.println(" 3. View Shopping List");
+				System.out.println(" 4. View Ingredients List for a day");
 				System.out.println(" 0. Exit");
-				//printWeek(w1);
+
 				switch (input.nextInt()) {
 					case 1:
-						System.out.println("Menu:");
-						Week w = generateNewMenu(f);
+						System.out.println("New Menu:");
+						w = generateNewMenu(f);
+						printWeek(w);
 						break;
 					case 2:
-						System.out.println("Not implemented yet");
+						if(w!=null) {
+							System.out.println("Menu:");
+							printWeek(w);
+						}else
+							System.out.println("**No Menu Created Yet!**");
+						break;
+					case 3:
+						if(w!=null) {
+							System.out.println("Shopping List:");
+							System.out.println(w.getShoppingList().toString());
+						}else
+							System.out.println("**No Menu Created Yet!**");
+						break;
+					case 4:
+						if(w!=null) {
+							System.out.println("What day?(1-7)");
+							int num = input.nextInt();
+							if(w.getDay(num-1).getMeal()!=null)
+								System.out.println(w.getDay(num-1).getMeal());
+							else
+								System.out.println("*No Meal*");
+						}else
+							System.out.println("**No Menu Created Yet!**");
 						break;
 					case 0:
 						System.out.println("Goodbye!");
 						active = false;
 						break;
 					default:
-						System.out.println("invalid input");
+						System.out.println("Invalid input");
 						break;
 				}
 			}
+			input.close();
 			System.out.println("End Program");
-		}catch (Exception e) {
-			System.out.println("Main error");
-			System.out.println(e);
-			System.out.println(e.getStackTrace());
+		}catch (InputMismatchException e) {
+			System.out.println("ERROR:Invalid input");
+			System.out.println("System Restart...");
+			System.out.println();
+			System.out.println();
+			main(args);
+		}catch (Exception E) {
+			System.out.println("Program Terminated "+E);
 		}
-//		IngredientsList l1 = new IngredientsList();
-//		System.out.println(l1.getIngredientsMeasurmentType(0));
 	}
-
-	
-
 }
