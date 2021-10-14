0c5b32febdc80e0d800b84cf31a3eabc1861d1c9
DallenCorry <89269364+DallenCorry@users.noreply.github.com>
2021-10-13 18:16:43 -0600

    Update Week.java

--- Final-Project/Week.java
+++ Final-Project/Week.java
@@ -1,17 +1,34 @@
 package calendar;
 
-import java.util.ArrayList;
-
+/**
+ * Creates a Week with 7 Day objects and a Shopping list Object.
+ * @author Dallen Corry
+ * @version 1.1
+ * @since 10/13/2021
+ */
 public class Week {
 	Day[] days = new Day[7];
-	ArrayList<String> shoppingList = new ArrayList<String>();
+	ShoppingList shoppingList;
 	
+	/**
+	 * Default Constructor
+	 */
 	public Week() {
 		for (int i =0; i<days.length; i++)
 			days[i]= new Day();
 		shoppingList = null;
 	}
 	
+	/**
+	 * Creates a week from 7 given days
+	 * @param d1 (Day) day 1
+	 * @param d2 (Day) day 2
+	 * @param d3 (Day) day 3
+	 * @param d4 (Day) day 4
+	 * @param d5 (Day) day 5
+	 * @param d6 (Day) day 6
+	 * @param d7 (Day) day 7
+	 */
 	public Week(Day d1, Day d2, Day d3, Day d4, Day d5, Day d6, Day d7) {
 		days[0]=d1;
 		days[1]=d2;
@@ -20,50 +37,59 @@ public class Week {
 		days[4]=d5;
 		days[5]=d6;
 		days[6]=d7;
-		//set the shopping list equal to the ingredients of all days
-		shoppingList = createShoppingList(days);
+		shoppingList = new ShoppingList(this);
+	}
+	/**
+	 * Creates a week from an array of Day objects
+	 * @param d (Day[]) The array of Days that will create the Week
+	 */
+	public Week(Day[] d) {
+		for (int i=0; i<days.length;i++)
+			days[i] = d[i];
+		shoppingList = new ShoppingList(this);
 	}
 
-	
-	private ArrayList<String> createShoppingList(Day[] d) {
-		ArrayList<String> newList = new ArrayList<String>();
-		return newList;
-	}
-	/*
-	 * I need to make a list of all the ingredients and their quantities types and names.
-	 * Names will be hardest. How do I check if chicken, pulled chicken, and canned chicken are the same?
-	 * search through the whole names list, and the index of that is what needs to be added with the existing thing.
-	 * 		say that each recipe will never have more than 1 occurrence of an ingredient
-	 * 
-	 * so if I find chicken in meal 1, then I add Chicken to the List. 
-	 * then i go through all of meal 2, and if I find anything that contains "chicken" I add the numbers together in the List. 
-	 * 	But keep the name just "chicken 
-	 * 
-	 * But what if in Meal 1 2 and 3 they all say different things than just chicken? 
-	 * 		I could make it so you can only input names that fit with my categories but that would mean I need to check each input.
-	 * 		Or I could have a giant list of names and check against it when I create my List.
-	 * */
-
-	public String mealName(int i, int m) {// m is for meal number, not yet implemented (ie, one day has multiple meals)
+	/**
+	 * Gets the name of a meal for a specific day of the week
+	 * @param i (Day) Day of the week to get meal from, indexed 0-6 inclusive
+	 * @param m (int) Unused currently (for later implementation of multiple Meals a Day)
+	 * @return (String) The name of a Meal for Day i.
+	 */
+	public String mealName(int i, int m) {// m is for meal number, not yet implemented (ie, one day will have multiple meals)
 		if(days[i]!=null&&days[i].getMeal()!=null)
 			return days[i].getMeal().getName();
 		return "No Meal";
 	}
-	
+	/**
+	 * Gets the IngredientsList object for a specified day 
+	 * @param i (int) The index of the Day of the Week you want to get the Meal of.
+	 * @return the IngredientsList object of the Meal of the selected Day of the Week.
+	 */
+	public IngredientsList getIngredients(int i) {
+		return days[i].getMeal().getIngredients();
+	}
+	/**
+	 * Gets a specified Day from a week.
+	 * @param i (int) The index of the day to be gotten (0-6 inclusive).
+	 * @return (Day) The specified Day object.
+	 */
 	public Day getDay(int i) {
 		return days[i];
 	}
-
-	public void setDay(int i, Day day) {
-		days[i] = day;
+	/**
+	 * Sets the day to a specified Day
+	 * @param i (int) The index of the day to be set (0-6 inclusive).
+	 * @param d (Day) the Day object to be set
+	 */
+	public void setDay(int i, Day d) {
+		days[i] = d;
 	}
-
-	public String getShoppingList(int i) {
-		return shoppingList.get(i);
-	}
-
-	public void setShoppingList(int i, String item) {
-		shoppingList.set(i, item);
+	/**
+	 * Gets the Shopping list for a week
+	 * @return (ShoppingList) the Shopping List object for this week.
+	 */
+	public ShoppingList getShoppingList() {
+		return shoppingList;
 	}
 	
 	@Override
@@ -73,12 +99,14 @@ public class Week {
 			s+= days[i].toString();
 		return s;
 	}
-	
+	/**
+	 * Creates a String with the date and a meal without ingredients.
+	 * @return a string representation of the Day without the ingredients from the Meal.
+	 */
 	public String toStringWithoutIngredients() {
 		String s = "";
 		for(int i=0; i<days.length; i++)
 			s+= days[i].toStringWithoutIngredients();
 		return s;
 	}
-
 }
